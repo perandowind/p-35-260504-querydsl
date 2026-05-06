12bfe63612297c1cd1691225d1f6f29719f91f78
perandowind <perandowind@gmail.com>
2026-05-06 14:27:14 +0900

    15

--- backend/src/main/kotlin/com/back/domain/member/repository/MemberRepositoryCustom.kt
+++ backend/src/main/kotlin/com/back/domain/member/repository/MemberRepositoryCustom.kt
@@ -9,4 +9,7 @@ interface MemberRepositoryCustom {
     fun findQByUsernameAndNickname(username: String, nickname: String): Member?
     fun findQByUsernameOrNickname(username: String, nickname: String): List<Member> // Or이므로 여러개 반환가능
     fun findQByUsernameAndEitherPasswordOrNickname(username: String, password: String, nickname: String): List<Member>
+    fun findQByNicknameContaining(nickname: String): List<Member>
+    fun countQByNicknameContaining(nickname: String): Long
+    fun existsQByNicknameContaining(nickname: String): Boolean
 }
